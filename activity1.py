c8379bda0b758af008c4f38f8e5aee80e8876194
Elvis Tony <elvistonyrency@gmail.com>
2024-05-28 21:13:07 +0100

    Update activity1.py

--- activity1.py
+++ activity1.py
@@ -2,11 +2,16 @@
 
 # Integer
 counter = 15
+print("Counter:",counter)
 # Float
 discount = 1.5
+print("Discount:",counter)
 # String
 name = "Mr Beast"
+print("Name:",counter)
 # Boolean
 sunnyDay = False
+print("Is it Sunny:",counter)
 # List
 shopping = ["Banana","Eggs","Bread"]
+print("Shopping List:",counter)
